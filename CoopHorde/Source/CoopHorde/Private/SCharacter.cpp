62ff6bb9314e140acff8a19b9692e96994151fd8
cnpatton <botanicality@gmail.com>
2018-10-17 21:11:04 -0500

    Added weapon blueprint and placeholder mesh

--- CoopHorde/Source/CoopHorde/Private/SCharacter.cpp
+++ CoopHorde/Source/CoopHorde/Private/SCharacter.cpp
@@ -77,6 +77,6 @@ void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 	PlayerInputComponent->BindAction("Crouch", IE_Pressed, this, &ASCharacter::BeginCrouch);
 	PlayerInputComponent->BindAction("Crouch", IE_Released, this, &ASCharacter::EndCrouch);
 
-	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ASCharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
 }
 
